27e4d891180294cdf149f4e6fc357a68585a0e19
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-06-17 15:40:31 +1200

    Finished testing nodes

--- src/main/java/interpreter/BinaryNodeTest.java
+++ src/main/java/interpreter/BinaryNodeTest.java
@@ -31,4 +31,37 @@ public class BinaryNodeTest {
 
     }
 
+    @Test
+    public void testBinaryNode() {
+        NumToken a = new NumToken(56.12);
+        NumToken b = new NumToken(34.34);
+        NumNode a_node = new NumNode(a);
+        NumNode b_node = new NumNode(b);
+        Token plus = new Token(TokenType.PLUS);
+        BinaryNode c = new BinaryNode(a_node, plus, b_node);
+        System.out.println(c);
+        String expected = "(56.12[PLUS]34.34)";
+        assertEquals(c.toString(), expected);
+
+    }
+
+    @Test
+    public void testCombo() {
+        NumToken a = new NumToken(56.12);
+        NumNode a_node = new NumNode(a);
+        UnaryNode b_unary = new UnaryNode(new Token(TokenType.MINUS), new NumNode(new NumToken(89.23)));
+        Token multiply = new Token(TokenType.STAR);
+        BinaryNode c = new BinaryNode(a_node, multiply, b_unary);
+        System.out.println(c);
+        String expected = "(56.12[STAR]([MINUS]89.23))";
+        assertEquals(c.toString(), expected);
+
+        BinaryNode lhs = new BinaryNode(new NumNode(new NumToken(32,0)), new Token(TokenType.PLUS), new NumNode(new NumToken(10)));
+        BinaryNode rhs = c;
+        BinaryNode resultNode = new BinaryNode(lhs, new Token(TokenType.MINUS) ,rhs);
+        System.out.println(resultNode);
+        expected = "((32.0[PLUS]10.0)[MINUS](56.12[STAR]([MINUS]89.23)))";
+        assertEquals(resultNode.toString(), expected);
+    }
+
 }
